0fb1475d74dbf3d7d542a0d4a93b60141e129397
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-26 07:55:51 -0600

    Created framework for CSP project, and changed Gitignore files.

---
+++ mitchMcEntire_CSP/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 10/26/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
