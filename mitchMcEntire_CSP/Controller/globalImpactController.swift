443c68338382cb16f239562721bf20fe3ab3bfc5
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-26 08:09:54 -0600

    Created all Big Idea controllers and made them inherit from UIViewController.

---
+++ mitchMcEntire_CSP/Controller/globalImpactController.swift
@@ -0,0 +1,35 @@
+//
+//  globalImpactController.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 10/26/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class globalImpactController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ mitchMcEntire_CSP/Controller/programmingController.swift
@@ -0,0 +1,23 @@
+//
+//  programmingController.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 10/26/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class programmingController: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
