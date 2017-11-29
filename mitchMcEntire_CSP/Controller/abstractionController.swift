cff845c5a33267aa259c357246e7a30cc7cddeaf
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-11-29 08:27:58 -0700

    Attempted to make the abstraction page work and the internet split page work at the same time. Currently struggling hard.

--- mitchMcEntire_CSP/Controller/abstractionController.swift
+++ mitchMcEntire_CSP/Controller/abstractionController.swift
@@ -8,18 +8,113 @@
 
 import UIKit
 
-class abstractionController: UIViewController
+class abstractionController: UIViewController, UIPageViewControllerDataSource
 {
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-    }
-    
-    override func didReceiveMemoryWarning()
-    {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        //MARK: Array of subviews
+        private (set) lazy var orderedAbstractionViews : [UIViewController] =
+        {
+            return [
+                self.newAbstractionViewController(abstractionLevel: "Block"),
+                self.newAbstractionViewController(abstractionLevel: "Java"),
+                self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+                self.newAbstractionViewController(abstractionLevel: "Binary"),
+                self.newAbstractionViewController(abstractionLevel: "AndGate")
+            ]
+        }()
+        
+        //Helper method to retrieve the correct ViewController
+        private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+        {
+            return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(abstractionLevel)ViewController")
+        }
+        
+        override func viewDidLoad()
+        {
+            super.viewDidLoad()
+            dataSource = self
+            
+            if let viewController = orderedAbstractionViews.first
+            {
+                setViewController [viewController],
+                                   direction: .forward,
+                                   animated: true,
+                                   completion: nil)
+            }
+        }
+        
+        //MARK:- Required protocol methods for UIPageViewControllerDataSource
+        public func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+                else
+            {
+                return nil
+            }
+            
+            let previousIndex = viewControllerIndex - 1
+            
+            guard previousIndex >= 0
+                else
+            {
+                return orderedAbstractionViews.last
+            }
+            
+            guard orderedAbstractionViews.count > previousIndex
+                else
+            {
+                return nil
+            }
+            
+            return orderedAbstractionViews[previousIndex]
+        }
+        
+        public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+                else
+            {
+                return nil
+            }
+            
+            let nextIndex = viewControllerIndex + 1
+            
+            guard nextIndex >= 0
+                else
+            {
+                return nil
+            }
+            
+            guard nextIndex < orderedAbstractionViews.count
+                else
+            {
+                return orderedAbstractionViews.first
+            }
+            
+            return orderedAbstractionViews[nextIndex]
+        }
+        
+        //MARK:- Support for dots in the UIPageViewController
+        
+        public func presentationCount(for pageViewController: UIPageViewController) -> Int
+        {
+            return orderedAbstractionViews.count
+        }
+        
+        public func presentationIndex(for pageViewController: UIPageViewController) -> Int
+        {
+            guard let firstViewController = ViewControllers?.first, let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+                else
+            {
+                return 0
+            }
+            
+            return viewControllerIndex
+        }
+        
+        override func didReceiveMemoryWarning()
+        {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+        }
 }
