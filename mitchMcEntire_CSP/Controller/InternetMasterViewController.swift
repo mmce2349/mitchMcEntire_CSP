c82b85d67e19112e32f17fb0a458fc119799e71a
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-11-29 08:41:23 -0700

    Added details to the master internet view controller

--- mitchMcEntire_CSP/Controller/InternetMasterViewController.swift
+++ mitchMcEntire_CSP/Controller/InternetMasterViewController.swift
@@ -11,26 +11,56 @@ import UIKit
 class InternetMasterViewController: UITableViewController
 {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private (set) lazy var internetTopics : [String] =
+    {
+        return [
+            "Definitions",
+            "CSP",
+            "CTEC",
+            "Canyons",
+            "Twitter",
+            "Start Guide",
+            ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
     }
-
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        
+        self.clearsSelectionOnViewWillAppear = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - Table view data source
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-    */
-
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueResuableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel.text = currentText
+        
+        return cell
+    }
 }
