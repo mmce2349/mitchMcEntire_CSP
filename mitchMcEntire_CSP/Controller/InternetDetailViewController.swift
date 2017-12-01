9ebd2e6256dd22d9cb37241b4f849ee9229efb19
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-12-01 09:15:33 -0700

    Added tons of code to allow access to internet and fixed my webviewer

--- mitchMcEntire_CSP/Controller/InternetDetailViewController.swift
+++ mitchMcEntire_CSP/Controller/InternetDetailViewController.swift
@@ -11,19 +11,66 @@ import WebKit
 class InternetDetailViewController: UIViewController
 {
     @IBOutlet weak var textView: UILabel!
+    @IBOutlet weak var webView: WKWebView!
     
-    @IBOutlet weak var webView: UIWebView!
-    override func viewDidLoad() {
+    
+    var detailAddress:String?
+    {
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        configureDetailView()
 
         // Do any additional setup after loading the view.
+    }
+   
+    private func configureDetailView() -> Void
+    {
+        if detailAddress != nil
+        {
+            if let currentWebView = webView
+            {
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        }
+        else
+        {
+            if let currentWebView = webView
+            {
+                let currentURL = URL(string: "https://www.cnn.com")
+                currentWebView.load(URLRequest(url:currentURL!))
+            }
+        }
+        
+        if detailText != nil
+        {
+            if let currentText = textView
+            {
+                currentText.text = detailText
+            }
+        }
+        else
+        {
+            if let currentText = textView
+            {
+                currentText.text = "Mitch's CSP app internet screen"
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 
     /*
     // MARK: - Navigation
