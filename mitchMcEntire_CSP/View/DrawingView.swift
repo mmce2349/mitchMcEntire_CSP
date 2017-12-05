2291689e491c590d2f54e779c8e5f70c9306f3d5
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-12-05 09:23:45 -0700

    Added a happy tree drawing to my drawing view

--- mitchMcEntire_CSP/View/DrawingView.swift
+++ mitchMcEntire_CSP/View/DrawingView.swift
@@ -44,10 +44,37 @@ class DrawingView: UIView {
         
         return stickFigure
     }
-    private func drawTurtle() -> Void
-   {
-    }
+   
     private func drawHappyTree() ->Void
+    {
+        let bobRoss = UIBezierPath()
+        
+        bobRoss.move(to: CGPoint(x:80, y:50))
+        bobRoss.addLine(to:CGPoint(x:120, y: 150))
+        bobRoss.addLine(to: CGPoint(x:40, y:150))
+        bobRoss.close()
+        UIColor(patternImage: UIImage(named: "Cute")!).setFill()
+        UIColor.brown.setStroke()
+        bobRoss.lineWidth = 2.0
+        bobRoss.fill()
+        bobRoss.stroke()
+        
+        let happyTree = UIBezierPath()
+        
+        UIColor.green.setFill()
+        happyTree.move(to: CGPoint(x: 110, y:150))
+        happyTree.addLine(to: CGPoint(x: 150, y:200))
+        happyTree.addLine(to: CGPoint(x: 10, y:200))
+        happyTree.addLine(to: CGPoint(x: 50, y:150))
+        happyTree.close()
+        happyTree.stroke()
+        happyTree.fill()
+        happyTree.move(to: CGPoint(x: 80, y:200))
+        happyTree.addLine(to: CGPoint(x: 80, y: 250))
+        happyTree.lineWidth = 6.0
+        happyTree.stroke()
+    }
+    private func drawTurtle() -> Void
     {
     }
 }
