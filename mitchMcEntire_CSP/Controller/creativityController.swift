8fff9e650539f26fe60248652cb73e0761874cfc
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-12-07 07:51:58 -0700

    added my  art to creativity page

--- mitchMcEntire_CSP/Controller/creativityController.swift
+++ mitchMcEntire_CSP/Controller/creativityController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class creativityController: UIViewController
+class creativityController: UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
+    private let reuseIDentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow: CGFloat = 3
+    
+    private lazy var artSelection : [UIImage?] =
+    {
+        return [
+            UIImage(named: "cute"),
+            UIImage(named: "myArt")
+        
+        ]
+    }()
+    
+    //var largePhotoIndexPath: IndexPath?
     override func viewDidLoad()
     {
         super.viewDidLoad()
