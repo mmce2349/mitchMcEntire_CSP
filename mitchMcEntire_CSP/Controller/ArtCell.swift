b05574cbcd516bb8794461da9f0d30bec2929fb4
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-12-07 09:03:28 -0700

    added artcell file

---
+++ mitchMcEntire_CSP/Controller/ArtCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArtCell.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 12/7/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell: UICollectionViewController
+{
+    
+    
+ 
+}
