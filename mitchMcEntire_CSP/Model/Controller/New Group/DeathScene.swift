0d159c4223ea8b4116014d1cc36e4fb30e99da83
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-12-19 08:39:33 -0700

    Added a game over scene

---
+++ mitchMcEntire_CSP/Model/Controller/New Group/DeathScene.swift
@@ -0,0 +1,41 @@
+//
+//  DeathScene.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 12/19/17.
+//  Copyright © 2017 Mcentire, Mitchell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+class DeathScene : SKScene
+{
+    override public func didMove(to view: SKView) -> Void
+    {
+        self.backgroundColor = SKColor.green
+        
+        let gameOver = SKLabelNode(text: "You have died!")
+        gameOver.name = "game over"
+        gameOver.fontSize = 65
+        gameOver.fontColor = SKColor.red
+        gameOver.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        addChild(gameOver)
+    }
+    
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) -> Void
+    {
+        let touch = touches.first
+        let touchLocation = touch?.location(in:self)
+        let touchedNode = self.atPoint(touchLocation!)
+        if(touchedNode.name == "game over")
+        {
+            let newGameScene = GameScene(size: size)
+            newGameScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontal(withDuration: 0.5)
+            view?.presentScene(newGameScene,transition: transitionType)
+        }
+    }
+}
