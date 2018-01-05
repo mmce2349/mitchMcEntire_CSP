d83456353e056459cb4628cab8b2f9ab3d3bd94f
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-05 09:27:09 -0700

    Added files for Data page

---
+++ mitchMcEntire_CSP/Model/BucketItem.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItem.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 1/5/18.
+//  Copyright © 2018 Mcentire, Mitchell. All rights reserved.
+//
+
+import Foundation
