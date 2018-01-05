865b2078ec1132489c09737e035e9a381882461f
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-01-05 09:20:52 -0700

    Started to work on the DataView project

---
+++ mitchMcEntire_CSP/Model/Controller/DataView/BucketItemCell.swift
@@ -0,0 +1,14 @@
+//
+//  BucketItemCell.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 1/5/18.
+//  Copyright © 2018 Mcentire, Mitchell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class BucketItemCell : UITableViewCell
+{
+    
+}
---
+++ mitchMcEntire_CSP/Model/Controller/DataView/DataViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DataViewController.swift
+//  mitchMcEntire_CSP
+//
+//  Created by Mcentire, Mitchell on 1/5/18.
+//  Copyright © 2018 Mcentire, Mitchell. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataViewController : UITableViewController
+{
+    
+}
